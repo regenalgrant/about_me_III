d4e646bd74e83e5092b9d03c2d2e635b2e825411
regenalgrant <regenal@mac.com>
2016-09-22 09:26:05 -0700

    styles.css

---
+++ styles.css
@@ -0,0 +1,33 @@
+
+header
+{
+	text-align: center;
+  background: red;
+  color: #fff;
+  padding: 10px 10px 10px 10px;
+}
+
+header h2
+{
+  text-align: center;
+  background: blue;
+  color: #fff;
+  padding: 10px 10px 10px 10px;
+
+  margin-bottom: 10px;
+}
+
+body
+ {
+  background: #666;
+}
+
+a
+{
+  color: #c79ecb;
+}
+
+main li
+{
+  color: #00b233
+}
